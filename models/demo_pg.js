26e99b5a96478c8528f87a1e61abdc5403f98543
Mariangelica Rodriguez <gromarant@gmail.com>
2023-06-13 16:57:31 +0200

    Delete release() from demo_pg.js

--- models/demo_pg.js
+++ models/demo_pg.js
@@ -5,10 +5,6 @@ pool.connect((err, client, release) => {
     return console.error('Error acquiring client', err.stack)
   }
   client.query('SELECT NOW()', (err, result) => {
-<<<<<<< HEAD
-    // release()
-=======
->>>>>>> develop
     if (err) {
       return console.error('Error executing query', err.stack)
     }
